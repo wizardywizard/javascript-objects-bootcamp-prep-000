ec50eb5ac4db7c5a0c0be5e4c0b55ee103eadc05
David Little <littledavid175@gmail.com>
2019-06-02 17:44:27 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,6 +3,6 @@ var playlist={
   song: 'thunderstruck'
 }
 function updatePlaylist (playlist,artistName,songTitle) {
- var updatePlaylist={'Slowdive':'Alison','My Bloody Valentine':
+ var updatePlaylist={'Slowdive':'Alison','My Bloody Valentine':'Sometimes','Phil Ochs':""
   return updatePlaylist
 }
