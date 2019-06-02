c831bdb6dca77f59cbc10e2032abaf882bbefe28
David Little <littledavid175@gmail.com>
2019-06-02 17:28:11 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,7 +3,7 @@ var playlist={
   song: 'thunderstruck'
 }
 function updatePlaylist (playlist,artistName,songTitle) {
-  updatePlaylist[artistName] = 'Phil Ochs'
-  updatePlaylist[songTitle] = 'My Bloody Valentine'
+  updatePlaylist[artistName] = 'Phil Ochs';
+  updatePlaylist[songTitle] = 'My Bloody Valentine';
   
 }
