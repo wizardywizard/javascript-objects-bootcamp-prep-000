0f3a1cc6f303dbedc24d924b204da9298b21d73a
David Little <littledavid175@gmail.com>
2019-06-02 18:15:32 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,6 +3,6 @@ var playlist={
   song: 'thunderstruck'
 }
 function updatePlaylist (playlist,artistName,songTitle) {
- 
+playlist['Phile Ochs'] = "here's to the State of Mississippi"
   return updatePlaylist
 }
